6740267e2d72b66f77fc2ea6c02873ea14834ee3
Tejasvi Sharma <tejasvisharma@gwu.edu>
2018-11-21 18:55:44 -0500

    flaskapp updated to handle multiple indexes

--- NLP_project/flaskapp.py
+++ NLP_project/flaskapp.py
@@ -8,12 +8,26 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    es,index_id=elastic_object()
-    print(index_id["ach"])
-    output=es.get(index=processed_text.lower(), doc_type="_doc",id=index_id[processed_text.lower()])
-    return(output["_source"]["Pages"])
+	text = request.form['text']
+	processed_text = text.split(",")
+	es,index_id=elastic_object()
+	pages_list=''
+	for i in processed_text:
+		output=es.get(index=i.lower(), doc_type="_doc",id=index_id[i.lower()])
+		pages_list=pages_list+","+output["_source"]["Pages"]
+	pages=pages_list.split(",")
+	data=""
+	for i in pages:
+		if(i!=""):
+			data=data+"<br><h1>"+i+"</h1><br>"
+			data=data+read_file(i)
+
+	return(data)
+    
+
+def read_file(filename):
+	input_file_text = open(filename , encoding='utf-8').read()
+	return input_file_text
 
 def elastic_object():
 	from elasticsearch import Elasticsearch, RequestsHttpConnection
@@ -40,6 +54,13 @@ def elastic_object():
 		key=i.split(",")[0]
 		value=int(i.split(",")[1].replace("\n",""))
 		index_id[key]=value
+
+	data1=open("index_file1.csv","r").readlines()
+	for j in data1:
+		key=j.split(",")[0]
+		value=int(j.split(",")[1].replace("\n",""))
+		index_id[key]=value
+	
 	return es,index_id
 
 
