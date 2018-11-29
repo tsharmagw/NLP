92a0551a6b6ae818110387862cf1fca0226c0320
Tejasvi Sharma <tejasvisharma@gwu.edu>
2018-11-29 04:58:00 -0500

    New pages added and css improved

--- NLP_project/flaskapp.py
+++ NLP_project/flaskapp.py
@@ -1,10 +1,63 @@
 from flask import Flask, request, render_template
+import re
 
 app = Flask(__name__)
 
 @app.route('/')
 def my_form():
     return render_template('form.html')
+
+@app.route('/jargon')
+def my_jargon():
+	jargons_list=[]
+	data1=open("index_file1.csv","r").readlines()
+	for j in data1:
+		key=j.split(",")[0]
+		jargons_list.append(key)
+
+	data="<!DOCTYPE html><html><head>"
+	data=data+"<title align=center>"+"Jargons List"+"</title>"
+	data=data+"<h2 align=center>Jargons List</h2>"
+	data=data+"<table style=width:100%>"
+	for i in range(0,len(jargons_list),10):
+		j=i
+		data=data+"<tr>"
+		while(j<(i+10) and j< len(jargons_list)):
+			data=data+"<td>"+jargons_list[j]+"</td>"
+			j=j+1
+		data=data+"</tr>"
+
+
+	data=data+"</table>"
+	data=data+"</head></html>"
+    # return render_template('jargon.html')
+	return(data)
+
+@app.route('/abbr')
+def my_abbr():
+	data=open("index_file.csv","r").readlines()
+	abbr_list=[]
+	for i in data:
+		key=i.split(",")[0]
+		abbr_list.append(key)
+
+	data="<!DOCTYPE html><html><head>"
+	data=data+"<title align=center>"+"Abbreviation List"+"</title>"
+	data=data+"<h2 align=center>Abbreviation List</h2>"
+	data=data+"<table style=width:100%>"
+	for i in range(0,len(abbr_list),10):
+		j=i
+		data=data+"<tr>"
+		while(j<(i+10) and j<len(abbr_list)):
+			data=data+"<td>"+abbr_list[j]+"</td>"
+			j=j+1
+		data=data+"</tr>"
+
+
+	data=data+"</table>"
+	data=data+"</head></html>"
+    # return render_template('jargon.html')
+	return(data)
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -19,14 +72,23 @@ def my_form_post():
 	data=""
 	for i in pages:
 		if(i!=""):
-			data=data+"<br><h1>"+i+"</h1><br>"
-			data=data+read_file(i)
+			start_index_list=[m.start() for m in re.finditer("/",i)]
+			data=data+"<br><h1>"+"Page Name: "+i[start_index_list[-2]+1:start_index_list[-1]]+"</h1><br>"
+			data=data+read_file(i,processed_text)
 
 	return(data)
     
 
-def read_file(filename):
+def read_file(filename,word_list):
 	input_file_text = open(filename , encoding='utf-8').read()
+	for i in word_list:
+		start_index_list=[m.start() for m in re.finditer(i,input_file_text,re.IGNORECASE)]
+		if(len(start_index_list)>0):
+			a=0
+			for j in start_index_list:
+				index=j+a*13
+				input_file_text=input_file_text[:index]+"<mark>"+input_file_text[index:index+len(i)]+"</mark>"+input_file_text[index+len(i):]
+				a=a+1
 	return input_file_text
 
 def elastic_object():
